676c6041cb7adde0289e155af2b10a48e3780440
Robert Leggett <robert.leggett2@auspost.com.au>
2016-11-22 12:38:14 +1100

    Added testing with mocha Added sinon for spy, mock and stubbing Added basic test and controller to handle restful endpoint

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # node_restful_webservice
+
+This does not require bable to perform transpiling as later version of node have ES6 support natively, however current version of node used here does not support import so require is being used.
+
+Requires
+========
+
+Node: v6.9.1
+
+Testing
+=======
+
+npm test
+
+Running
+=======
+
+npm start
+
+### The node server is set to run on
+
+**http://localhost:3000**
---
+++ test/customer/CustomerController.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const sinon = require('sinon');
+
+const customerController = require('../../lib/customer/CustomerController');
+
+describe('Customer Controller', () => {
+  describe('get', () => {
+    const resStub = {};
+
+    beforeEach(() => {
+      resStub.status = sinon.stub().returns(resStub);
+      resStub.json = sinon.stub().returns(resStub);
+    });
+
+    it('should return 200 successful', () => {
+      const req = {};
+
+      customerController.get(req, resStub);
+
+      assert(resStub.status.calledWith(200));
+      assert(resStub.json.calledWith({}));
+    });
+  });
+});
