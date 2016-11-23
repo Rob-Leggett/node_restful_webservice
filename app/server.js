5dcb27681dec4b1086160efe1d9e2baf3de0c413
Robert Leggett <robert.leggett2@auspost.com.au>
2016-11-23 13:29:32 +1100

    Added connect to mongoldb via mongoose

--- app/server.js
+++ app/server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
+const config = require("./configuration/config");
 const authRouter = require("./auth/authRouter");
 const customerRouter = require("./customer/customerRouter");
 const exceptionHandler = require("./exceptionHandler");
+
+// ######### Database Connection ###########
+
+mongoose.connect(config.database); // connect to database
+
+// ######### Express Application ###########
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
@@ -28,3 +36,6 @@ app.use(exceptionHandler.handle);
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
 });
+
+// https://scotch.io/tutorials/authenticate-a-node-js-api-with-json-web-tokens
+// https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
