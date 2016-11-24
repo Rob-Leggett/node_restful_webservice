69a998b265a7d2fdbba60fd22f45ff56850dedec
Robert Leggett <robert.leggett2@auspost.com.au>
2016-11-24 17:23:29 +1100

    Trying to fix files not appearing in git

--- app/user/company.js
+++
@@ -1,8 +0,0 @@
-// get an instance of mongoose and mongoose.Schema
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-// set up a mongoose model and pass it using module.exports
-module.exports = mongoose.model('Company', new Schema({
-  name: String
-}));
